135d0f476a2c75e071aae5f5b2b3356e41ff30ab
Ayannah Bryant <ayannahbbryant@gmail.com>
2025-05-24 13:41:30 -0400

    Assignment 4 - Python Basics - Reversing a string

---
+++ PY100_PythonBasics/reverse.py
@@ -0,0 +1,5 @@
+#To slice an object you use variable_name[start:stop:step]
+#if start and stop are empty the method will iterate over the entire object/string
+
+string = 'hello'
+print(string[::-1])
