f0fecc70bb3360009f9fdbfaf0783555bcc74ddd
Ayannah Bryant <ayannahbbryant@gmail.com>
2025-05-24 16:05:56 -0400

    Added if condition to function

--- PY100_PythonBasics/list.py
+++ PY100_PythonBasics/list.py
@@ -1,6 +1,7 @@
 def access_second_element(x): 
-    second_item = x[1]
-    return second_item
+    if len(x) > 1:
+        second_item = x[1]
+        return second_item
 
 list_a = ['fish', 'and', 'chips']
 print(access_second_element(list_a))
