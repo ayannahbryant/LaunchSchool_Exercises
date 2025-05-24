4334656b46305be895851ff3c1d4b79210740f61
ayannahbryant <ayannahbbryant@gmail.com>
2025-05-23 22:33:15 -0400

    Update Python_Docs.py
    Added new line

--- PY100_PythonBasics/Python_Docs.py
+++ PY100_PythonBasics/Python_Docs.py
@@ -3,6 +3,7 @@
 #Python library reference - https://docs.python.org/3/library/index.html
 #Python Glossary - https://docs.python.org/3/glossary.html
 #Python Complete Table of Contents - https://docs.python.org/3/contents.html
+
 #Python's comprehensive library reference detailing Python's built-in modules, functions, classes,
 #information about Python's syntax, standard library, and development guides.
 
