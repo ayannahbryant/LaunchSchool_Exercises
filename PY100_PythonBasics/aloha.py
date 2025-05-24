a19850b68a76f7721861ba925348dd94ebd4f68c
Ayannah Bryant <ayannahbbryant@gmail.com>
2025-05-23 23:37:11 -0400

    Adding additional commit information

--- PY100_PythonBasics/aloha.py
+++ PY100_PythonBasics/aloha.py
@@ -1,5 +1,6 @@
 #In Python, you can use the .lower() to lowercase a string
 #The method you need is part of the built-in str class.
+#The method str.lower() returns a copy of str with all uppercase characters converted to lowercase.
 
 string = 'Aloha, World!'
 print(string.lower())
