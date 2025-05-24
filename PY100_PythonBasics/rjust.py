62a8fc73f7f5043134e194fbfd361d909f1fccb5
Ayannah Bryant <ayannahbbryant@gmail.com>
2025-05-24 00:18:02 -0400

    Added commits about width requirements

--- PY100_PythonBasics/rjust.py
+++ PY100_PythonBasics/rjust.py
@@ -1,3 +1,10 @@
 #The Python method to right justify a string is: str.rjust()
+'''str.rjust(width[, fillchar])
+Return the string right justified in a string of length width. Padding is done using the specified fillchar 
+(default is an ASCII space). The original string is returned if width is less than or equal to len(s).
+
+Width must be greater than the length of the string
+'''
+
 string = 'Align to the right!'
 print(string.rjust(10,'_'))
