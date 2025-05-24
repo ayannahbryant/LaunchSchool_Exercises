0b16847ab7889202c279cf9461aa543ae527d3c4
Ayannah Bryant <ayannahbbryant@gmail.com>
2025-05-24 16:40:06 -0400

    Assignment 6 - Python Basics - Finding the Index

--- PY100_PythonBasics/index.py
+++ PY100_PythonBasics/index.py
@@ -1,7 +1,8 @@
+#Allows for resuable code for any set of list
+
 def access_fruit(x,y):
     fruit_index = x.index(y)
     return print(fruit_index)
-
 
 fruits = ["apple", "banana", "cherry", "peach", "watermelon"]
 access_fruit(fruits,'cherry')
