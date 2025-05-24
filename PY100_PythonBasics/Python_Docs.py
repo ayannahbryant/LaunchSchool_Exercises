7cfadd75fd5ac55ca0302ef464b5346052622cb1
Ayannah Bryant <ayannahbbryant@gmail.com>
2025-05-23 21:47:21 -0400

    Assignment 1 - Python Basics - Reading Docs

---
+++ PY100_PythonBasics/Python_Docs.py
@@ -0,0 +1,6 @@
+#The Python Official documentation - https://docs.python.org/3/
+#Python tutorials 
+#Python library reference 
+#Python's comprehensive library reference detailing Python's built-in modules, functions, classes,
+#information about Python's syntax, standard library, and development guides.
+
