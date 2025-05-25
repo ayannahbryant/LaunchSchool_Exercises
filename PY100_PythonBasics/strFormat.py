7118ebc5c7f2ae6f1cebc0ba8285e233009081bf
Ayannah Bryant <ayannahbbryant@gmail.com>
2025-05-25 16:24:31 -0400

    Assignment 1-2 - String Formating

---
+++ PY100_PythonBasics/strFormat.py
@@ -0,0 +1,6 @@
+name = "Victor"
+profession = "programmer"
+
+print('Hello, {}. You are a {}.'.format(name,profession)) #use brackets as placeholders 
+
+print(f'Hello, {name}. You are a {profession}.')
