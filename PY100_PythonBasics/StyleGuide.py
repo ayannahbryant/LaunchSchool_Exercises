0a958d860fb592ad4196b3028fe207e1cbcc236e
Ayannah Bryant <ayannahbbryant@gmail.com>
2025-05-25 17:00:03 -0400

    Assignment 2 - 2: Style Guide

---
+++ PY100_PythonBasics/StyleGuide.py
@@ -0,0 +1,15 @@
+iceCreamDensity=10 #Only Class names should use the CapWords convention and use space around operators for readability
+
+while iceCreamDensity>0 : #Only Class names should use the CapWords convention and use space around operators for readability
+    print('Drip...')
+    iceCreamDensity-=1 #Only Class names should use the CapWords convention and use space around operators for readability
+
+print('The ice cream melted.')
+
+ice_cream_density = 10
+
+while ice_cream_density > 0:
+    print('Drip...')
+    ice_cream_density -= 1
+
+print('The ice cream melted.')
