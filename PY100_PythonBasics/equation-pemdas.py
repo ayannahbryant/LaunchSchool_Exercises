7eceb9fbbba94277106574c82de991f82174caa8
Ayannah Bryant <ayannahbbryant@gmail.com>
2025-05-25 19:34:54 -0400

    Assignment 3 - 2 - Order of Operation

---
+++ PY100_PythonBasics/equation-pemdas.py
@@ -0,0 +1,14 @@
+#The order of operation is as follows PEMDAS: 
+#Expressions in the parentheses are solved first 
+#Next, exponents are solved next 
+#Then, multiplication and division are solved from left to right 
+#Last, addition and subtraction are solved from left to right 
+
+#To solve this equation, the program would: 
+#Solve 3**2 = 9 (4 * 5 + 9 / 10)
+#Solve 4 * 5 = 20 (20 + 9 / 10)
+#Solve 9 / 10 = 0.9 (20 + 0.9)
+#Solve 20 + 0.9 = 20.9
+
+equation_result = 4 * 5 + 3**2 / 10
+print(equation_result)
