6fdb0830ec413eb7811ba3d291259e2a7a43f892
Ayannah Bryant <ayannahbbryant@gmail.com>
2025-05-24 21:39:26 -0400

    Assignment 6 & 7 - List index and Out of range

--- PY100_PythonBasics/list.py
+++ PY100_PythonBasics/list.py
@@ -1,3 +1,7 @@
+#If the function tries to access an element at position 10 and the list 
+#does not have an element at position 10, based on list_a, it will 
+#throw an exception IndexError.
+
 def access_second_element(x): 
     if len(x) > 1:
         second_item = x[1]
---
+++ PY100_PythonBasics/list2.py
@@ -0,0 +1,16 @@
+#If the function tries to access an element at position 10 and the list 
+#does not have an element at position 10, based on list_a, it will 
+#throw an exception IndexError.
+    
+def access_element(items_list, target_item): 
+    if len(items_list) == 0:
+        return 'List is empty'
+    elif target_item < len(items_list):
+        second_item = items_list[target_item]
+        return second_item
+    else:
+        return 'Item is out of range'
+        
+
+dinner = ['fish', 'and', 'chips']
+print(access_element(dinner, 10))
